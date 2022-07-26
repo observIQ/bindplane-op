ba92dec7e37c13e8762992a54764145c7f3a5c43
David VanLaningham <david.vanlaningham@bluemedora.com>
2022-07-26 13:43:59 -0400

    Handle error on session.Save

--- internal/server/auth/session.go
+++ internal/server/auth/session.go
@@ -29,12 +29,17 @@ func CheckSession(server server.BindPlane) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session, err := server.Store().UserSessions().Get(c.Request, sessions.CookieName)
 		if err != nil {
-			// Clear the cookie, this can happen when sessions-secrets are changed
-			// and a cookie with the previous secret is read.
+			// Clear the cookie, this can happen when sessions-secrets change
+			// and we see a cookie with the previous secret is read.
 			session.Options.MaxAge = 0
-			session.Save(c.Request, c.Writer)
 
-			c.AbortWithError(http.StatusInternalServerError, err)
+			err := session.Save(c.Request, c.Writer)
+			if err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+
+			c.AbortWithError(http.StatusUnauthorized, err)
 			return
 		}
 
